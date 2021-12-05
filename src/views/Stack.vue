ff2862f69a22d99674bd61dd588d45442d3bc797
eMagixe <max.magixe@gmail.com>
2021-12-05 20:49:28 +0500

    create description in stack

--- src/views/Stack.vue
+++ src/views/Stack.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
       <div v-for="group in stacks" :key="group.id">
-          <h2 class="is-size-3 m-3">{{ group.name }}</h2>
+          <h2 class="is-size-3 mt-5">{{ group.name }}</h2>
+          <p>{{ group.description }}</p>
           <stack-list :items="group.items"></stack-list>
       </div>
   </div>
